18e272b54e2a043352404ef9fa3be129b141aec6
viira <74811857+viira@users.noreply.github.com>
2021-01-17 11:51:46 -0800

    Replace all tabs
    Replace all tabs with spaces

--- RPro/Display/Swapchain.h
+++ RPro/Display/Swapchain.h
@@ -12,24 +12,24 @@
 
 namespace Display::Swapchain
 {
-	struct Details
-	{
-		VkFormat imageFormat;
-		VkExtent2D extent;
-	};
+    struct Details
+    {
+        VkFormat imageFormat;
+        VkExtent2D extent;
+    };
 
-	VkSwapchainKHR create(GLFWwindow* window,
-						  const VkDevice &device,
-						  const VkSurfaceKHR &surface,
-						  const Display::Surface::SupportDetails
-						  &surfaceSupportDetails,
-						  const Device::Queues::FamilyIndices
-						  &queueFamilyIndices,
-						  Display::Swapchain::Details
-						  &swapchainDetails);
+    VkSwapchainKHR create(GLFWwindow* window,
+                          const VkDevice &device,
+                          const VkSurfaceKHR &surface,
+                          const Display::Surface::SupportDetails
+                          &surfaceSupportDetails,
+                          const Device::Queues::FamilyIndices
+                          &queueFamilyIndices,
+                          Display::Swapchain::Details
+                          &swapchainDetails);
 
-	std::vector<VkImage> retrieveImages(const VkDevice& device,
-										const VkSwapchainKHR& swapchain);
+    std::vector<VkImage> retrieveImages(const VkDevice& device,
+                                        const VkSwapchainKHR& swapchain);
 };
 
 #endif
